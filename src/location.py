91f48a963a0e4d359a8a1593859a78ad876bd5c8
tncrdk <85764203+tncrdk@users.noreply.github.com>
2023-03-23 18:38:55 +0100

    fjernet litt ubrukte imports og fikset kode etter formatteringsendring

--- src/location.py
+++ src/location.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 import numpy as np
-import matplotlib.pyplot as plt
-from pathlib import Path
 from geopy import distance
 
 # earth semi- and major axis. se main.ipynb
-a = distance.ELLIPSOIDS['WGS-84'][0]
-b = distance.ELLIPSOIDS['WGS-84'][1]
+a = distance.ELLIPSOIDS["WGS-84"][0]
+b = distance.ELLIPSOIDS["WGS-84"][1]
+
 
 def geocentric_radius(phi: float) -> float:
     """
@@ -20,13 +19,14 @@ def geocentric_radius(phi: float) -> float:
     -----
     radius: float. Geocentric radius
     """
-    numer = (a**2 * np.cos(phi))**2 + (b**2 * np.sin(phi))**2
-    denom = (a*np.cos(phi))**2 + (b * np.sin(phi))**2
-    radius = np.sqrt(numer/denom)
+    numer = (a**2 * np.cos(phi)) ** 2 + (b**2 * np.sin(phi)) ** 2
+    denom = (a * np.cos(phi)) ** 2 + (b * np.sin(phi)) ** 2
+    radius = np.sqrt(numer / denom)
     return radius
 
+
 def angle_north(d_phi, d_theta) -> float:
-    alpha = np.arctan(d_theta/d_phi)
+    alpha = np.arctan(d_theta / d_phi)
     return alpha
 
 
@@ -36,4 +36,4 @@ def convert_to_rad(coord: list) -> None:
 
 
 def distance_geopy(p1, p2):
-    return distance.geodesic(p1, p2, ellipsoid='WGS-84').m
+    return distance.geodesic(p1, p2, ellipsoid="WGS-84").m
